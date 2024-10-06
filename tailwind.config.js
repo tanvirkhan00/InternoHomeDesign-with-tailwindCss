764358b091665365bce41d232e55384f2cb44525
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-10-06 16:44:40 +0600

    correcting project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm:"480px",
+      md:"768px",
+      lg:"1024px"
+    },
+    extend: {
+      colors: {
+        tillColor:'#008080',
+        darkPurpelColor:'#301934',
+        yellowColor:'#DFFF00',
+        midnightBlueColor:'#191970',
+        whiteColor:'#fff',
+        blackColor:'#000',
+        greenColor:'#007936',
+        redColor:'#cc3433',
+        darkColor:'#000',
+        darkColorLight:'#171717',
+        limeColor:'#00FF00',
+        blueColor:'#0000FF',
+        skyBlueColor:'#87CEEB',
+        brownColor:'#A52A2A',
+        coffeeColor:'#6F4E37',
+        grayColor:'#A9A9A9',
+        platinumColor:'#E5E4E2',
+        darkPinkColor:'#AA336A'
+      },
+      backgroundImage: {
+        "homeHiro": "url('/Image/Image.png')",
+      },
+    },
+    container:{
+      center:true,
+      padding: {
+        DEFAULT:'1rem',
+        sm: '1.5rem',
+      }
+    },
+    fontFamily:{
+      fontSuse: ["SUSE", 'sans-serif'],
+      fontProtest: ["Protest Guerrilla", 'sans-serif'],
+      fontLobster: ["Lobster", 'sans-serif'],
+      fontBebas: ["Bebas Neue", 'sans-serif'],
+      fontDancingScript: ["Dancing Script", 'sans-serif'],
+      fontSpicyRice: ["Spicy Rice", 'sans-serif']
+    }
   },
   plugins: [],
 }
