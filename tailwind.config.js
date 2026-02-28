8d52c1d40088dfa5629679580d7495902ac25405
tanvirkhan00 <kmtanvir1111@gmail.com>
2026-02-28 16:03:23 +0600

    enhance home, about, service and project page Ui and function

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     screens: {
       sm:"480px",
       md:"768px",
-      lg:"1024px"
+      lg:"1024px",
+      xl:"1280px",
+      xxl:"1536px"
     },
     extend: {
       colors: {
